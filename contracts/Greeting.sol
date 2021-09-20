2a8467d4329fed639cd38adacca97c539164b368
Defi Academy <72668835+DefiAcademy@users.noreply.github.com>
2021-09-20 14:21:33 +0100

    Update Greeting.sol

--- contracts/Greeting.sol
+++ contracts/Greeting.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.3;
+pragma solidity ^0.8.7;
 
 contract Greeting {
-    function sayHello() public pure returns(bytes32) {
+    function sayHello() external pure returns (string memory) {
         return "hello world";
     }
 }
