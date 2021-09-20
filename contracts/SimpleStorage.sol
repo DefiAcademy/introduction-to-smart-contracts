e75ba4670b7160d2cb7ded59a953f75f36acfc07
Defi Academy <72668835+DefiAcademy@users.noreply.github.com>
2021-09-20 22:04:44 +0100

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.3;
+pragma solidity ^0.8.7;
 
 contract SimpleStorage {
-    uint public age;
+    uint8 public age;
     
     constructor() {
         age = 10;
