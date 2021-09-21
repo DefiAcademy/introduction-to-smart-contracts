bcf79af75159fac977eb7469ecf95377353d3042
Defi Academy <72668835+DefiAcademy@users.noreply.github.com>
2021-09-21 16:27:09 +0100

    Update AdvanceStorage.sol

--- contracts/AdvanceStorage.sol
+++ contracts/AdvanceStorage.sol
@@ -3,14 +3,11 @@
 pragma solidity ^0.8.7;
 
 contract AdvanceStorage {
+
     mapping(address => uint8) public ages;
     
-    function register(uint8 age) public returns(bool){
+    function register(uint8 age) external returns(bool) {
         ages[msg.sender] = age;
         return true;
-    }
-    
-    function getAge(address anyAddress) public view returns(uint8){
-        return ages[anyAddress];
     }
 }
