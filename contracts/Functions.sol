4b03b060586be2cd93c3df973c77620fed3a41bb
Defi Academy <72668835+DefiAcademy@users.noreply.github.com>
2021-09-22 11:00:32 +0100

    Update Functions.sol

--- contracts/Functions.sol
+++ contracts/Functions.sol
@@ -4,24 +4,32 @@ pragma solidity ^0.8.7;
 
 contract Functions {
     uint256 public data;
+    uint256 public multiplier;
     address public owner;
     
-    constructor() {
-        data = 10;
+    constructor(uint256 initialValue, uint256 initialMultiplier) {
+        data = initialValue;    
+        multiplier = initialMultiplier;
         owner = msg.sender;
     }
     
-    function setComputedData(uint256 _newData) external onlyOwner{
-        data = computeData(_newData);
-        setData(data);
-    }
-
-    function setData(uint256 _newData) public onlyOwner{
-        data = _newData;
+    function setMultiplier(uint256 newMultiplier) external onlyOwner returns(bool){
+        multiplier = newMultiplier;
+        return true;
     }
     
-    function computeData(uint256 _data) internal pure returns (uint256){
-        return _data * 2;
+    function setData(uint256 newData) public onlyOwner returns(bool) {
+        data = newData;
+        return true;
+    }
+    
+    function setComputedData(uint256 newData) external onlyOwner returns(bool) {
+        uint256 dataComputed = computeData(newData);
+        return setData(dataComputed);
+    }
+    
+    function computeData(uint256 someData) internal view returns(uint256) {
+        return someData * multiplier; 
     }
     
     modifier onlyOwner {
